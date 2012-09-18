85e8035de080c9759c072655fe5320f77a0b0546
ryukimin <ryukimin@34884bfb-9b71-6305-1ad6-60af5512e408>
2012-09-18 05:24:15 +0000

    회원가입 & 로그인 추가

--- blog/src/main/java/kr/co/webcash/repository/UserRepository.java
+++ blog/src/main/java/kr/co/webcash/repository/UserRepository.java
@@ -8,4 +8,8 @@ public interface UserRepository {
 	
 	public List<User> findAll();
 
+	public void insert(User user);
+
+	public User findByLoginId(String loginId);
+
 }
--- blog/src/main/java/kr/co/webcash/web/user/UserController.java
+++ blog/src/main/java/kr/co/webcash/web/user/UserController.java
@@ -1,10 +1,34 @@
 package kr.co.webcash.web.user;
 
+import kr.co.webcash.domain.User;
+import kr.co.webcash.service.UserService;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
+	
+	@Autowired private UserService userService;
+	
+	@RequestMapping("/register/step01")
+	public void step01(){
+		
+	}
+	@RequestMapping(value="/register/register", method=RequestMethod.POST)
+	public String register(@RequestParam String loginId, @RequestParam String password, @RequestParam String name){
+		User user = new User();
+		user.setLoginId(loginId);
+		user.setPassword(password);
+		user.setName(name);
+		
+		userService.save(user);
+		
+		return "redirect:/";
+	}
 
 }
