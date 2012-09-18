69258f4d3ea5b037b92f6414add80fd1af7b1923
kwakyc87 <kwakyc87@34884bfb-9b71-6305-1ad6-60af5512e408>
2012-09-18 01:50:38 +0000

    로그인, 로그아웃 추가

---
+++ blog/src/main/java/kr/co/webcash/domain/LoginUser.java
@@ -0,0 +1,17 @@
+package kr.co.webcash.domain;
+
+public class LoginUser {
+	
+	private String loginId;
+
+	public String getLoginId() {
+		return loginId;
+	}
+
+	public void setLoginId(String loginId) {
+		this.loginId = loginId;
+	}
+	
+	
+
+}
---
+++ blog/src/main/java/kr/co/webcash/web/user/UserController.java
@@ -0,0 +1,10 @@
+package kr.co.webcash.web.user;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+}
