3ac1724543f95ba22ab92ef7f0f3aaafbc518084
kwakyc87 <kwakyc87@34884bfb-9b71-6305-1ad6-60af5512e408>
2012-09-19 07:53:14 +0000

    샘플 데이터 추가

--- trunk/blog/src/main/resources/schema-visitor.sql
+++ trunk/blog/src/main/resources/schema-visitor.sql
@@ -7,3 +7,15 @@ CREATE TABLE visitor (
   PRIMARY KEY(id, blogId)
 );
 
+insert into visitor (id, writer, blogId, contents, dateCreated)
+values ('1', '2', '1', '안녕하세요. 들렀다가 갑니다.', now());
+
+insert into visitor (id, writer, blogId, contents, dateCreated)
+values ('2', '1', '1', '공지사항 - 블로그 이사합니다.', now());
+
+insert into visitor (id, writer, blogId, contents, dateCreated)
+values ('1', '1', '2', '안녕하세요. 저도 들렀다가 갑니다.', now());
+
+insert into visitor (id, writer, blogId, contents, dateCreated)
+values ('2', '2', '2', '롤로로로로로롤', now());
+
