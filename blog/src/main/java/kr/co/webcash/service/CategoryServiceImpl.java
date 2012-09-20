5824f82a02f23f0d5c77d732054f56a21fa9f631
ryukimin <ryukimin@34884bfb-9b71-6305-1ad6-60af5512e408>
2012-09-20 04:49:09 +0000

    관리자 페이지  - 카테고리 설정 (추가/수정/삭제) 기능 추가

---
+++ blog/src/main/java/kr/co/webcash/service/CategoryServiceImpl.java
@@ -0,0 +1,49 @@
+package kr.co.webcash.service;
+
+import java.util.List;
+
+import kr.co.webcash.domain.Blog;
+import kr.co.webcash.domain.Category;
+import kr.co.webcash.domain.Post;
+import kr.co.webcash.repository.CategoryRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CategoryServiceImpl implements CategoryService {
+	@Autowired private CategoryRepository categoryRepository;
+	@Override
+	public List<Category> listByBlogId(String blogId) {
+		return categoryRepository.findAllByBlogId(blogId);
+	}
+
+	@Override
+	public int findLastIdByBlogId(String blogId) {
+		Category category = categoryRepository.findLastPostByBlogId(blogId);
+
+		return Integer.parseInt(category.getId());
+	}
+
+	@Override
+	public void save(Category category) {
+		categoryRepository.insert(category);
+	}
+
+	@Override
+	public void update(Category category) {
+		categoryRepository.update(category);
+	}
+
+	@Override
+	public void delete(Category category) {
+		categoryRepository.delete(category);
+	}
+
+	@Override
+	public Category findByIdAndBlogId(String id, String blogId) {
+		// TODO Auto-generated method stub
+		return categoryRepository.findByIdAndBlogId(id,blogId);
+	}
+
+}
---
+++ blog/src/main/webapp/WEB-INF/views/userblog/admin/category/add.jsp
@@ -0,0 +1,12 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+
+<jsp:include page="/WEB-INF/views/common/header.jsp" />
+
+<div>
+	<form action="${pageContext.request.contextPath }/${loginUser.loginId }/admin/category/addAction" method="POST">
+		<label>Title </label><input type="text" name="title"  /> <br />
+		<input type="submit" value="추가"  />
+	</form>
+</div>
+
+<jsp:include page="/WEB-INF/views/common/footer.jsp" />
