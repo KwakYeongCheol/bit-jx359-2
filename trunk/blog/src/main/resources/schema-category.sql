7a1c14ac0a04614c7ae9a65fea8f1060df1514df
ryukimin <ryukimin@34884bfb-9b71-6305-1ad6-60af5512e408>
2012-09-20 01:42:24 +0000

    오타 수정 및 블로그 요구사항

--- trunk/blog/src/main/resources/schema-category.sql
+++ trunk/blog/src/main/resources/schema-category.sql
@@ -2,6 +2,7 @@ CREATE TABLE category (
   id VARCHAR(10) NOT NULL,
   blogId VARCHAR(10) NOT NULL,
   title VARCHAR(50) NOT NULL,
+  primary key(id, blogId)
 );
 
 insert into category(id, blogId, title) values(1, 1, '2012년 일기');
