eaf0032aaac9864ea5bd9e276efd954d215e6eca
kwakyc87 <kwakyc87@34884bfb-9b71-6305-1ad6-60af5512e408>
2012-09-20 04:29:49 +0000

    카테고리 글만 볼 수 있도록 추가 /{blogId}/category/{categoryId}

---
+++ trunk/blog/src/main/java/kr/co/webcash/web/userblog/category/CategoryController.java
@@ -0,0 +1,28 @@
+package kr.co.webcash.web.userblog.category;
+
+import kr.co.webcash.service.CategoryService;
+import kr.co.webcash.service.PostService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/{blogId}/category")
+public class CategoryController {
+	
+	@Autowired private PostService postService;
+	@Autowired private CategoryService categoryService;
+	
+
+	@RequestMapping("/{categoryId}")
+	public String home(Model model, @PathVariable String blogId, @PathVariable String categoryId){
+		model.addAttribute("categoryList", categoryService.findAllByBlogId(blogId));
+		model.addAttribute("postList", postService.listByBlogIdAndCategoryId(blogId, categoryId));
+		
+		return "/userblog/category/home";
+	}
+
+}
